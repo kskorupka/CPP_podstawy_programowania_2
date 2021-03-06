f02cd2e77770f4d062660261ea602b67407119bf
Kamila Skorupka <kskorupka@student.agh.edu.pl>
2021-03-06 13:18:34 +0100

    .

--- Lab_1/build/src/CMakeFiles/run_template.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_template"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_template.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
