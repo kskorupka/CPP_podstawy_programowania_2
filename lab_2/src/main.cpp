876c417da59d8800ed8ce83936368bd88a853f25
Kamila Skorupka <kskorupka@student.agh.edu.pl>
2021-03-10 08:51:31 +0100

    .

--- lab_2/src/main.cpp
+++ lab_2/src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <math.h>
+class Circle{
+    public:
+    double r;
+    static double PI(){
+        return M_PI;
+    }
+    double Perimeter(double r){
+        return 2*PI()*r;
+    }
+    double Field(double r){
+        return pow(r,2)*PI();
+    }
+};
+class Square{
+    public:
 
-int main(int argc, char *argv[])
-{
-    std::cout << "Hello world." << std::endl;
+};
+int main(int argc, char *argv[]){
     return 0;
 }
