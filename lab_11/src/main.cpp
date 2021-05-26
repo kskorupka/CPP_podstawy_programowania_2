1bc2bc99d5324f7c7f6ef67257475b72d274b5f6
Kamila Skorupka <kskorupka@student.agh.edu.pl>
2021-05-26 08:50:30 +0200

    .

--- lab_11/src/main.cpp
+++ lab_11/src/main.cpp
@@ -219,56 +219,5 @@ VectoredList& VectoredList:: operator=(const VectoredList& v) {
     return *this;
 }
 int main() {
-    cout << endl << "---------- 1,2 ----------" << endl;
-     VectoredList v;
-     for (int i = 0; i < 101; ++i)
-         v.push_back("s" + to_string(i));
-
-     for (VectoredList::VectoredListIterator ita(v, 101); ita.good(); --ita)
-     {
-         cout << ita.get() << " ";
-         if (ita.cursor % 10 == 0)
-             cout << endl;
-     }
-
-    cout << endl << "---------- 3 ----------" << endl;;
-     for (const auto &element : v)
-         cout << element << "AAA" << endl;
-
-    cout << endl << "---------- 4 ----------" << endl;
-     VectoredList::VectoredListIterator it3(v, 3);
-     VectoredList::VectoredListIterator it33(v, 33);
-     VectoredList::VectoredListIterator it45(v, 45);
-
-     v.erase(it3);
-     v.pop_back();
-     v.pop_back();
-     v.erase(it33, it45);
-     for (int i = 0; i < v.VectoredListSize(); ++i)
-     {
-         cout << v[i] << " ";
-         if (i % 10 == 0)
-             cout << endl;
-     }
-     cout << endl << "---------- 5 ----------" << endl;
-     VectoredList v2 = v; //deep_copy
-      for (int i = 0; i < v2.VectoredListSize(); ++i)
-      {
-          cout << v2[i] << " ";
-          if (i % 10 == 0)
-              cout << endl;
-      }
-
-      // i've created operator=() overload too, but in this case we're using deep_copy constructor
-      //this also works:
-      /*
-      VectoredList v3;
-      v3 = v;
-      cout<<endl<<endl;
-      for (int i = 0; i < v3.VectoredListSize(); ++i)
-      {
-          cout << v3[i] << " ";
-          if (i % 10 == 0)
-              cout << endl;
-      }*/
+    
 }
